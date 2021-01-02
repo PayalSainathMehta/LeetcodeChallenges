ed4e46a2e007b8cf87e21f61c7b040b6c44a54d5
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2021-01-02 19:29:55 +0530

    Basic Calculator 1 and 2

--- src/BasicCalculator.java
+++ src/BasicCalculator.java
@@ -2,6 +2,9 @@ import java.util.Stack;
 
 public class BasicCalculator {
 
+    static Stack<Integer> stack = new Stack();
+    static int result = 0; //final result
+    static int number = 0; //temp
     static int evaluate(String s){
         //push in stack only when opening bracket and reset result and sign
         //pop from stack only when closing bracket and reset number
@@ -9,9 +12,7 @@ public class BasicCalculator {
         //for digit, calculate temp number
         //for +, calculate result, reset sign and number
         //for -, calculate result, reset sign and number
-        Stack<Integer> stack = new Stack();
-        int result = 0; //final result
-        int number = 0; //temp
+
         int sign = 1; // + , -
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
@@ -52,8 +53,42 @@ public class BasicCalculator {
         return result;
     }
 
+    //this is for basic calculator2 where we won't have paranthesis however we have * and /
+    static int evaluate2(String s){
+        stack = new Stack();
+        char sign = '+'; //we always push first time
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            char c = s.charAt(i);
+            boolean isDigit = Character.isDigit(c);
+            boolean isSpace = (c == ' ');
+            if(isDigit)
+                number = number * 10 + c - '0';
+            if(!isDigit && !isSpace || i == n - 1)
+            {
+                if(sign == '+')
+                    stack.push(number);
+                if(sign == '-')
+                    stack.push(-number);
+                if(sign == '*')
+                    stack.push(stack.pop() * number);
+                if(sign == '/')
+                    stack.push(stack.pop() * number);
+
+                sign = c;
+                number = 0;
+            }
+        }
+        for(int val: stack)
+            result += val;
+
+        return result;
+    }
+
     public static void main(String[] args) {
         String s = "(1+(4+5+2)-3)+(6+8)";
         System.out.println(evaluate(s));
+        String s2 = " 3/2 ";
+        System.out.println(evaluate2(s2));
     }
 }
